6ab5ebb425129eb97c221d49190f3581c8ff8f74
riaxe-satyabrata <satyabratap@riaxe.com>
2019-11-21 13:30:01 +0530

    env module updated according to the current changes

---
+++ config/settings.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ |--------------------------------------------------------------------------
+ | Global Configuration for the Application
+ |--------------------------------------------------------------------------
+ */
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'db' => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'inkxe_x',
+            'username'  => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../app/Views/',
+        ],
+        'pagination' => [
+            'per_page' => 40 // Default value for per page item showing
+        ],
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+        
+    ],
+];
