fee5dc4efc35beba91d32a634d78b740523f24b2
inkxe-Chandra <chandrakanta@riaxe.com>
2019-12-18 16:18:08 +0530

    minor changes in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,13 @@ var sourcemaps = require('gulp-sourcemaps');
 var exec2 = require('child_process').exec;
 var exec = require('gulp-exec');
 var os = require('os');
-const { src,dest,series,task,watch } = require('gulp');
+const {
+    src,
+    dest,
+    series,
+    task,
+    watch
+} = require('gulp');
 var gulp = require('gulp');
 var Prompt = require('prompt-checkbox');
 const envsettings = {};
@@ -16,6 +22,7 @@ const Postgrator = require('postgrator');
 const directoryExists = require('directory-exists');
 var jscrambler = require('gulp-jscrambler');
 const del = require('del');
+
 function gulp_init_settings(cb) {
     envsettings.data = JSON.parse(fs.readFileSync('./envsettings.json'));
     cb();
@@ -64,7 +71,7 @@ function apache_server_down(cb) {
 }
 
 function build_docker_package(cb) {
-    
+
     return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ && ' + envsettings.data.command_prefix + ' docker-compose build', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -74,7 +81,7 @@ function build_docker_package(cb) {
 
 
 function initiate_docker_server(cb) {
-    
+
     return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ && ' + envsettings.data.command_prefix + ' docker-compose up', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -83,8 +90,8 @@ function initiate_docker_server(cb) {
 }
 
 function pull_dockerfiles(cb) {
-    
-   return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ && ' + envsettings.data.command_prefix + ' docker-compose pull', function(err, stdout, stderr) {
+
+    return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ && ' + envsettings.data.command_prefix + ' docker-compose pull', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
     });
@@ -92,7 +99,7 @@ function pull_dockerfiles(cb) {
 }
 
 function stopxedocker(cb) {
-    
+
     return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ && ' + envsettings.data.command_prefix + ' docker-compose down && ' + envsettings.data.command_prefix + ' docker-compose stop', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -101,7 +108,7 @@ function stopxedocker(cb) {
 }
 
 function restartxedocker(cb) {
-    
+
     return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ && ' + envsettings.data.command_prefix + ' docker-compose restart', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -110,7 +117,7 @@ function restartxedocker(cb) {
 }
 
 function pullxeprojects(cb) {
-    
+
     return exec2('cd ' + envsettings.data.project_path + 'inkxe10-env/docker/ &&' + envsettings.data.command_prefix + ' docker exec -d php72 cp -a /var/www/html/xeprojects/. /var/xeprojects', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -129,7 +136,7 @@ function list_all_schema(cb) {
             return exec2(envsettings.data.command_prefix + ' mkdir ../schema/ && ' + envsettings.data.command_prefix + ' rm -rf ../schema/*.sql && ' + envsettings.data.command_prefix + ' cp -rf ../inkxe10-*/schema/*.sql ../schema/', function(err, stdout, stderr) {
                 console.log(stdout);
                 console.log(stderr);
-            });           
+            });
         }
     });
     cb();
@@ -170,7 +177,7 @@ function remove_unwanted_sql(cb) {
 }
 
 function schema_update(cb) {
-     const postgrator = new Postgrator({
+    const postgrator = new Postgrator({
         // Directory containing migration files
         migrationDirectory: '../out',
         validateChecksums: false, // Set to false to skip validation
@@ -221,78 +228,84 @@ function inkxe_admin(cb) {
 }
 
 function copy_inkxe_to_package(cb) {
-     return exec2(envsettings.data.command_prefix + ' cp -a ' + envsettings.data.project_path + '/inkxe10-designer-admin/dist/inkxe10-designer-admin/.' + ' ' + envsettings.data.project_path + "/" + envsettings.data.production_path + '/', function(err, stdout, stderr) {
+    return exec2(envsettings.data.command_prefix + ' cp -a ' + envsettings.data.project_path + '/inkxe10-designer-admin/dist/inkxe10-designer-admin/.' + ' ' + envsettings.data.project_path + "/" + envsettings.data.production_path + '/', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
     });
     cb();
 }
-function scramble_code(cb){
-    var jscrambler_data  = JSON.parse(fs.readFileSync('jscrambler.json'));
+
+function scramble_code(cb) {
+    var jscrambler_data = JSON.parse(fs.readFileSync('jscrambler.json'));
     return gulp
-    .src([envsettings.data.project_path+'/xetool/admin/*.js'])
-    .pipe(jscrambler(jscrambler_data))
-    .pipe(gulp.dest(envsettings.data.project_path+'/xetool/scrambled/'));
+        .src([envsettings.data.project_path + '/xetool/admin/*.js'])
+        .pipe(jscrambler(jscrambler_data))
+        .pipe(gulp.dest(envsettings.data.project_path + '/xetool/scrambled/'));
     cb();
-    }
-function move_scrambled_codes_to_main(cb){
-   return exec2( envsettings.data.command_prefix + ' cp -rf '+envsettings.data.project_path+'/xetool/xetool/admin/** ' +envsettings.data.project_path+'/xetool/admin/' , function(err, stdout, stderr) {
+}
+
+function move_scrambled_codes_to_main(cb) {
+    return exec2(envsettings.data.command_prefix + ' cp -rf ' + envsettings.data.project_path + '/xetool/xetool/admin/** ' + envsettings.data.project_path + '/xetool/admin/', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
     });
     cb();
 }
-function resetenv(cb){
+
+function resetenv(cb) {
     var platform = os.platform() === 'linux' ? 'ubuntu' : (
         os.platform() === 'darwin' ? 'mac' : (
-        os.platform() === 'win32' ? 'win' : 'win'));
-        console.log(platform);
-        return gulp.src('localsettings_'+ platform + '.json').pipe(concat('envsettings.json')).pipe(gulp.dest('./'));
-    cb(); 
-}
-function exec_drop_basic_db(cb){
-    var db_password       = (envsettings.data.db_password == '') ? '' : '-p' + envsettings.data.db_password;
-    console.log(envsettings.data.command_prefix +' mysql -h '+envsettings.data.db_host+' -u '+envsettings.data.db_user+' '+db_password+'  -e "DROP DATABASE IF EXISTS xe_install_db_inkxe_10";');       
-    return exec2(envsettings.data.command_prefix +' mysql -h '+envsettings.data.db_host+' -u '+envsettings.data.db_user+' -p '+db_password+'  -e "DROP DATABASE IF EXISTS xe_install_db_inkxe_10";', 
-        function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);    
-    });
+            os.platform() === 'win32' ? 'win' : 'win'));
+    console.log(platform);
+    return gulp.src('localsettings_' + platform + '.json').pipe(concat('envsettings.json')).pipe(gulp.dest('./'));
     cb();
 }
-function exec_create_basic_db(cb){
-    var db_password       = (envsettings.data.db_password == '') ? '' : '-p' + envsettings.data.db_password;
-    return exec2(envsettings.data.command_prefix +' mysql -h '+envsettings.data.db_host+' -u '+envsettings.data.db_user+' '+db_password+'  -e "CREATE DATABASE IF NOT EXISTS xe_install_db_inkxe_10";', 
-        function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);    
-    });
+
+function exec_drop_basic_db(cb) {
+    var db_password = (envsettings.data.db_password == '') ? '' : '-p' + envsettings.data.db_password;
+    return exec2(envsettings.data.command_prefix + ' mysql -h ' + envsettings.data.db_host + ' -u ' + envsettings.data.db_user + ' ' + db_password + '  -e "DROP DATABASE IF EXISTS xe_install_db_inkxe_10";',
+        function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
     cb();
 }
-function update_build_modules(cb){
-    return exec2(envsettings.data.command_prefix+' rm -rf  modules_list.txt && '+envsettings.data.command_prefix+'  echo '+envsettings.data.modules+' >> modules_list.txt', 
-    function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);    
-});
+
+function exec_create_basic_db(cb) {
+    var db_password = (envsettings.data.db_password == '') ? '' : '-p' + envsettings.data.db_password;
+    return exec2(envsettings.data.command_prefix + ' mysql -h ' + envsettings.data.db_host + ' -u ' + envsettings.data.db_user + ' ' + db_password + '  -e "CREATE DATABASE IF NOT EXISTS xe_install_db_inkxe_10";',
+        function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
     cb();
 }
-function clean_sql_directories(cb){
-    return exec2(envsettings.data.command_prefix +' rm -rf ../out/* ../schema/*', 
-    function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);    
-});
+
+function update_build_modules(cb) {
+    return exec2(envsettings.data.command_prefix + ' rm -rf  modules_list.txt && ' + envsettings.data.command_prefix + '  echo ' + envsettings.data.modules + ' >> modules_list.txt',
+        function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
     cb();
 }
-exports.xetool = series(gulp_init_settings,clean_sql_directories, delete_xetool,list_all_schema, xetool_apis, copy_xetool_vendor,rename_sql_files, sql_files_naming, merge_apis, copy_xetool_assets,exec_drop_basic_db, exec_create_basic_db, schema_update, create_basic_sql,inkxe_admin);
+
+function clean_sql_directories(cb) {
+    return exec2(envsettings.data.command_prefix + ' rm -rf ../out/* ../schema/*',
+        function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
+    cb();
+}
+exports.xetool = series(gulp_init_settings, clean_sql_directories, delete_xetool, list_all_schema, xetool_apis, copy_xetool_vendor, rename_sql_files, sql_files_naming, merge_apis, copy_xetool_assets, exec_drop_basic_db, exec_create_basic_db, schema_update, create_basic_sql, inkxe_admin);
 exports.start_docker = series(gulp_init_settings, build_docker_package, initiate_docker_server);
 exports.pullxedocker = series(gulp_init_settings, pull_dockerfiles);
 exports.stopxedocker = series(gulp_init_settings, stopxedocker);
 exports.restartxedocker = series(gulp_init_settings, restartxedocker);
 exports.pullxeprojects = series(gulp_init_settings, pullxeprojects);
-exports.createbasicsql = series(gulp_init_settings,list_all_schema,rename_sql_files, sql_files_naming,exec_drop_basic_db, exec_create_basic_db, schema_update, create_basic_sql);
-exports.scramblefiles  = series(gulp_init_settings,scramble_code,move_scrambled_codes_to_main);
-exports.resetenv       = series(resetenv);
-exports.update_build_modules = series(gulp_init_settings,update_build_modules);
-exports.test=series(gulp_init_settings,clean_sql_directories,list_all_schema,rename_sql_files);
+exports.createbasicsql = series(gulp_init_settings, list_all_schema, rename_sql_files, sql_files_naming, exec_drop_basic_db, exec_create_basic_db, schema_update, create_basic_sql);
+exports.scramblefiles = series(gulp_init_settings, scramble_code, move_scrambled_codes_to_main);
+exports.resetenv = series(resetenv);
+exports.update_build_modules = series(gulp_init_settings, update_build_modules);
+exports.test = series(gulp_init_settings, clean_sql_directories, list_all_schema, rename_sql_files);
