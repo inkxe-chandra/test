2aa99e4f4babf468571d6b2d35e5c8859a57ed09
riaxe-satyabrata <satyabratap@riaxe.com>
2019-12-18 14:45:16 +0530

    minor changes

--- schema/001.do.setting.sql
+++ schema/001.do.setting.sql
@@ -1,13 +1,3 @@
-CREATE TABLE `settings` (
-  `xe_id` int(11) NOT NULL AUTO_INCREMENT,
-  `setting_key` varchar(200) DEFAULT NULL,
-  `setting_value` varchar(200) DEFAULT NULL,
-  `autoload` tinyint(1) DEFAULT '0',
-  `is_secure` tinyint(1) DEFAULT '0',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`xe_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
-
 
 CREATE TABLE `languages` (
   `xe_id` int(10) NOT NULL AUTO_INCREMENT,
